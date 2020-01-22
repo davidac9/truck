f029d512c5f30abd58b1bb37b4bb0ff77c79855b
David Collett <davida.collett95@gmail.com>
2020-01-22 14:25:15 -0700

    added authController

---
+++ server/authController.js
@@ -0,0 +1,37 @@
+const bcrypt = require('bcrypt.js')
+
+module.exports = {
+    register: async (req, res) => {
+        const {username, password, profile_pic} = req.body
+        const db = req.app.get('db')
+        const checkName = await db.find_username([username])
+        if (checkName.length > 0) {
+            return res.status.send({message: 'Username in use'})
+        }
+        const salt = bcrypt.genSaltSync(10)
+        const hash = bcrypt.hashSync(password, salt)
+        const newUser = await db.insert_user({username, hash, profile_pic})
+        req.session.user = newUser[0]
+        return res.status(200).send({message: 'Logged in', user: req.session.user, loggedIn: true})
+    },
+    login: async (req, res) => {
+        const db = req.app.get('db')
+        const {username, password} = req.body
+        const user = await db.find_username([username])
+        if (user.length === 0) {
+            return res.status.send({message: 'Username not found'})
+        }
+        const result = bcrypt.compareSync(password, user[0].hash)
+        if (result) {
+            req.session.user = user[0]
+            return res.status(200).send({message: 'Logged in', user: req.session.user, loggedIn: true})
+        }
+    },
+    logout: async (req, res) => {
+        req.session.destroy()
+        res.status(200).send({message: 'Logged out', loggedIn: false})
+    },
+    authMe: async (req, res) => {
+        return res.status(200).send({message: 'component refreshed', user: req.session.user, loggedIn: true})
+    }
+}
